65a6dde5c749c8d7bfeb0562cf648670ed987913
mojoblue7 <lsh18542@gmail.com>
2020-05-05 04:01:07 +0900

    # 2020-05-05 # hooks-tutorial 디렉터리 생성

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter/Counter';
+import Info from './Info/Info';
+
+function App() {
+  return (
+    <div>
+      {/* <Counter></Counter> */}
+      <Info></Info>
+    </div>
+  );
+}
+
+export default App;
