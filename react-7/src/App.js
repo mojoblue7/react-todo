efa735efceac321293c6f73ed3b3e9ed432ee526
mojoblue7 <lsh18542@gmail.com>
2020-05-05 03:46:52 +0900

    # 2020-05-05 # 파일 디렉토리 구조 변경

--- react-7/src/App.js
+++ react-7/src/App.js
@@ -11,8 +11,23 @@ import RefSample from './RefSample/RefSample';
 import ScrollBox from './ScrollBox/ScrollBox';
 import IterationSample from './IterationSample/IterationSample';
 import LifeCycleSample from './LifeCycleSample/LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
     return (
       <div className='App'>
@@ -30,7 +45,10 @@ class App extends Component {
           맨 밑으로
         </button> */}
         {/* <IterationSample /> */}
-        <LifeCycleSample />
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+        <button onClick={this.handleClick}>랜덤 색상</button>
       </div>
     );
   }
