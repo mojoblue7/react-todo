926eb2ee73f05e232339d54fdcc04b3e555b83db
mojoblue7 <lsh18542@gmail.com>
2020-06-07 06:13:23 +0900

    # 2020-06-07 # todo에 immer 라이브러리 적용 # todo 함수 사용방법 변경 # style 변경

--- react-redux-tutorial/src/components/Counter.js
+++ react-redux-tutorial/src/components/Counter.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+const btnStyle = {
+  fontSize: '20px',
+};
+
 const Counter = ({ number, onIncrease, onDecrease }) => {
   return (
     <div>
-      <h1>{number}</h1>
-      <div>
-        <button onClick={onIncrease}>+1</button>
-        <button onClick={onDecrease}>-1</button>
+      <h1 style={{ textAlign: 'center' }}>{number}</h1>
+      <div style={{ textAlign: 'center' }}>
+        <button style={btnStyle} onClick={onIncrease}>
+          +1
+        </button>
+        <button style={btnStyle} onClick={onDecrease}>
+          -1
+        </button>
       </div>
     </div>
   );
