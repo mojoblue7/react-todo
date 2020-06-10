ee28a85dd6b92f3f9a93c3b0c0a6826caefd5595
mojoblue7 <lsh18542@gmail.com>
2020-06-10 21:11:42 +0900

    feature: commit for eject

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,22 +1,30 @@
 import React, { Component, Suspense, useState } from 'react';
 import logo from './logo.svg';
+import loadable from '@loadable/component';
 import './App.css';
-const SplitMe = React.lazy(() => import('./SplitMe'));
+
+// loadable 사용 부분
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>,
+});
 
 function App() {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
-
+  const onMouseOver = () => {
+    SplitMe.preload(); // 컴포넌트를 미리 불러옴
+  };
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p onClick={onClick}>Hello React!</p>
-        <Suspense fallback={<div>loading...</div>}>
-          {visible && <SplitMe />}
-        </Suspense>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          {/*onMouseOver시 컴포넌트를 미리 불러옴*/}
+          Hello React!
+        </p>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
